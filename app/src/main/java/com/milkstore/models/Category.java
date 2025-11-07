8f57d05e223a68533d5f8fb93cad0bba4c8aef20
phuchtq <taikhoanhoconl123@gmail.com>
2025-11-07 22:34:38 +0700

    Merge branch 'main' of D:\milkstore with conflicts.

--- app/src/main/java/com/milkstore/models/Category.java
+++ app/src/main/java/com/milkstore/models/Category.java
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName;
 
 import com.google.gson.annotations.SerializedName;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class Category implements Serializable {
